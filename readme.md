0fd6f64a8b1963ca5f32c2738f479d94995ab527
Guillaume Tazoeur Latour <g0latour@gmail.com>
2021-06-12 12:09:26 +0200

    init

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Covid OpenData Belgium
+
+## Task
+
+1. Analyze the evolution of covid-19 in Belgium since the beginning of the pandemics
+2. Check whether some profiles are affected more than others (age, region, socioeconomic background …)
+3. Analyze the effects of the covid-19 on the Belgian economy (bankruptcies, unemployment …)
+
+Then :
+
+4. generate a report (examples: power point, excel, power BI dashboard, Tableau dashboard)
+5. do a 10 minutes presentation
---
+++ utils/io.py
@@ -0,0 +1,93 @@
+def dataset_list():
+    return [
+        {
+            'file': 'covid-19-pandemic-belgium-hospitalisations-by-date-and-province.csv',
+            'name': 'COVID-19 Pandemic - Belgium - Hospitalisations by date and province',
+            'link': 'https://data.gov.be/en/dataset/covid-19-pandemic-belgium-hospitalisations-date-and-province'
+        },
+        {
+            'file': 'COVID19BE_CASES_AGESEX.csv',
+            'name': 'Confirmed cases by date, age, sex and province',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_CASES_MUNI_CUM.csv',
+            'name': 'Cumulative number of confirmed cases by municipality',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_HOSP.csv',
+            'name': 'Hospitalisations by date and provinces',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_TESTS.csv',
+            'name': 'Total number of tests by date',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_VACC_MUNI_CUM.csv',
+            'name': 'Administered vaccines by week, municipality, age and dose',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_CASES_MUNI.csv',
+            'name': 'Confirmed cases by date and municipality',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_MORT.csv',
+            'name': 'Mortality by date, age, sex, and region',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'COVID19BE_VACC.csv',
+            'name': 'Administered vaccines by date, region, age, sex, brand and dose',
+            'link': 'https://epistat.wiv-isp.be/covid'
+        },
+        {
+            'file': 'TF_VAT_NACE_EMPL_2019.sqlite',
+            'name': 'Enterprises subject to VAT according to employer class ',
+            'link': 'https://statbel.fgov.be/en/open-data/enterprises-subject-vat-according-employer-class-11'
+        },
+        {
+            'file': 'ermg-tables.xlsl',
+            'name': 'Impact du corona sur les entreprises belges',
+            'link': 'https://www.nbb.be/fr/articles/limpact-de-la-crise-du-coronavirus-sur-le-chiffre-daffaires-des-entreprises-belges-ne'
+        },
+        {
+            'file': 'Faill_Heb_fr.xslx',
+            'name': 'Nombre de faillites par semaine selon la région, l\'activité économique et la forme juridique',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/faillites/faillites-hebdomadaires#figures'
+        },
+        {
+            'file': 'TF_VAT_NACE_SQ_2019.zip',
+            'name': 'Nombre d’assujettis actifs à la TVA par classe d\'emploi et localisation du siège social',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-annuelle-des-entreprises-assujetties-la#panel-12',
+        },
+        {
+            'file': 'starters2017_fr.xlsx',
+            'name': 'Entreprises assujetties à la tva (2017)',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-mensuelle-des-entreprises-assujetties#figures'
+        },
+        {
+            'file': 'starters2018_fr.xlsx',
+            'name': 'Entreprises assujetties à la tva (2018)',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-mensuelle-des-entreprises-assujetties#figures'
+        },
+        {
+            'file': 'starters2019_fr.xlsx',
+            'name': 'Entreprises assujetties à la tva (2019)',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-mensuelle-des-entreprises-assujetties#figures'
+        },
+        {
+            'file': 'starters2020_fr.xlsx',
+            'name': 'Entreprises assujetties à la tva (2020)',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-mensuelle-des-entreprises-assujetties#figures'
+        },
+        {
+            'file': 'TF_STARTERS_45.xlsx',
+            'name': 'Entreprises assujetties à la tva (2021)',
+            'link': 'https://statbel.fgov.be/fr/themes/entreprises/entreprises-assujetties-la-tva/evolution-mensuelle-des-entreprises-assujetties#figures'
+        }
+    ]
